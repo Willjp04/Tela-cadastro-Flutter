3b21c1b79709cfc0a502d7744955c792f5f1fd19
Willian Sostenes <sostenes655@gmail.com>
2020-12-13 17:18:37 -0300

    Realizando testes com formfield

--- tela_flutter/lib/main.dart
+++ tela_flutter/lib/main.dart
@@ -10,11 +10,21 @@ class YabiApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Adicionar Usuario'),
-        ),
-      ),
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text('Adicionar Usuario'),
+          ),
+          body: Column(
+            children: <Widget>[
+              TextFormField(validator: (value) {
+                if (value.isEmpty) return "O campo é obrigatório";
+                if (value.length > 30)
+                  return "O campo não pode ter mais que 30 carácteres";
+                else
+                  return null;
+              })
+            ],
+          )),
     );
   }
 }
