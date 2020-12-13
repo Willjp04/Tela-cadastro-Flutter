7c4016b765f7f930017976b98d86d9b96fd3578f
Willian Sostenes <sostenes655@gmail.com>
2020-12-13 17:43:03 -0300

    Criando a view para adicionar usuario

--- tela_flutter/lib/main.dart
+++ tela_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tela_flutter/views/user_form.dart';
 
 main() {
   runApp(YabiApp());
@@ -9,22 +10,12 @@ class YabiApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title: Text('Adicionar Usuario'),
-          ),
-          body: Column(
-            children: <Widget>[
-              TextFormField(validator: (value) {
-                if (value.isEmpty) return "O campo é obrigatório";
-                if (value.length > 30)
-                  return "O campo não pode ter mais que 30 carácteres";
-                else
-                  return null;
-              })
-            ],
-          )),
+      title: 'Yabi App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: UserForm(),
     );
   }
 }
